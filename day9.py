1469b87e6caf60c393e80cae1442ddb0f69c8e08
lordraul <68354052+lordraul@users.noreply.github.com>
2024-12-10 10:32:46 -0500

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,57 @@
+import sys
+sys.setrecursionlimit(10000)
+
+def printFilemap(filemap):
+    line = ""
+    for id, length in filemap:
+        if id == -1:
+            line += "." * length
+        else:
+            line += str(id) * length
+    print(line)
+
+def fillFiles(filemap):
+    printFilemap(filemap)
+
+    for fileID, length in [f for f in filemap[::-1] if f[0] != -1]:
+        for i in range(filemap.index((fileID, length))):
+            s = filemap[i]
+            if s[0] == -1:
+                if s[1] > length:
+                    filemap[i] = (s[0], s[1] - length)
+                    filemap[filemap.index((fileID, length))] = (-1, length)
+                    filemap.insert(i, (fileID, length))
+                    # return fillFiles(filemap)
+                elif s[1] == length:
+                    filemap[filemap.index((fileID, length))] = (-1, length)
+                    filemap[i] = (fileID, length)
+                    # return fillFiles(filemap)
+                printFilemap(filemap)
+    return filemap
+                        
+
+with open("day9input.txt", "r") as input:
+    filemap = []
+
+    file = True
+    fileID = 0
+    for digit in input.readlines()[0]:
+        if file:
+            filemap.append((fileID, int(digit)))
+            fileID += 1
+        else:
+            filemap.append((-1, int(digit)))
+        file = not file
+
+    checksum = 0
+
+    filled = fillFiles(filemap)
+
+    i = 0
+    for fileID, length in filled:
+        if fileID != -1:
+            for n in range(i, i + length):
+                checksum += fileID * n
+        i += length
+
+    print(checksum)
